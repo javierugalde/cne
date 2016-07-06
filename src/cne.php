85e312ade029a66979a5fd1ba77f39ceae5812d7
Alexis Sanchez <aasanchez@gmail.com>
2016-07-06 17:05:22 +0200

    Update

--- src/cne.php
+++ src/cne.php
@@ -3,6 +3,7 @@
 namespace CNE;
 
 use GuzzleHttp\Client as Guzzle;
+use Symfony\Component\DomCrawler\Crawler as Crawler;
 
 class Cne
 {
@@ -14,51 +15,19 @@ class Cne
     public function search()
     {
         $client = new Guzzle();
-        $res = $client->request('GET', 'http://www.cne.gov.ve/web/registro_electoral/ce.php?nacionalidad=V&cedula=15777118');
+        $res = $client->request('GET', 'http://www.cne.gov.ve/web/registro_electoral/ce.php?nacionalidad=V&cedula=0001');
         $body = $res->getBody();
-        $data = $this->refactorData(strip_tags($body));
-
-        return json_encode($data);
-    }
-
-    private function refactorData($result)
-    {
-        if (strstr($result, 'DATOS DEL ELECTOR')) {
-            $look = array(
-                'Cédula:',
-                'Nombre:',
-                'Estado:',
-                'Municipio:',
-                'Parroquia:',
-                'Centro:',
-                'Dirección:',
-                'SERVICIO ELECTORAL',
-                'Mesa:',
-                'Imprimir',
-                'Cerrar',
-            );
-            $info = explode("@", trim(str_replace($look, '@', $result)));
-            $name = explode(" ", $info[2]);
-
-            return array(
-                "Status" => "OK",
-                "CI" => preg_replace('/(\v|\s)+/', ' ', trim($info[1])),
-                "Primer Nombre" => preg_replace('/(\v|\s)+/', ' ', trim($name[0])),
-                "Segundo Nombre" => preg_replace('/(\v|\s)+/', ' ', trim($name[1])),
-                "Primer Apellido" => preg_replace('/(\v|\s)+/', ' ', trim($name[count($name) - 2])),
-                "Segundo Apellido" => preg_replace('/(\v|\s)+/', ' ', trim($name[count($name) - 1])),
-                "Estado" => preg_replace('/(\v|\s)+/', ' ', trim($info[3])),
-                "Municipio" => preg_replace('/(\v|\s)+/', ' ', trim($info[4])),
-                "Parroquia" => preg_replace('/(\v|\s)+/', ' ', trim($info[5])),
-                "Centro" => preg_replace('/(\v|\s)+/', ' ', trim($info[6])),
-                "Servicio" => preg_replace('/(\v|\s)+/', ' ', trim($info[8])),
-            );
-        } else {
-            return array(
-                "Status" => "ERR",
-                "Description" => "La cédula no es valida, o el elector esta inhabilitado",
-            );
+        $crawler = new Crawler((string) $body);
+        $text = utf8_decode($crawler->text());
+        $text = explode("\n", $text);
+        $data =  [];
+        foreach ($text as $line){
+            $line = trim($line);
+            if($line != ""){
+                array_push($data,$line);
+            }
         }
+        var_dump($data);
 
     }
 }
