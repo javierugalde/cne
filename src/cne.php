51dde999c8a7f163558100264a3c6654f4cd856e
Alexis Sanchez <aasanchez@gmail.com>
2016-07-15 16:40:08 +0200

    UpdatE

--- src/cne.php
+++ src/cne.php
@@ -12,23 +12,58 @@ class Cne
     {
     }
 
-    public function search()
+    public function search($citizenship,$dni)
     {
+
+        $dni = $this->cleanDni($dni);
+        $dni = $this->completeDni($dni);
+
+        $citizenship=$this->cleanCitizenship($citizenship);
+        $query = http_build_query(["nacionalidad" => $citizenship, "cedula" => $dni]);
+        $output = [];
         $client = new Guzzle();
-        $res = $client->request('GET', 'http://www.cne.gov.ve/web/registro_electoral/ce.php?nacionalidad=V&cedula=0001');
-        $body = $res->getBody();
+        $response = $client->request('GET', 'http://www.cne.gov.ve/web/registro_electoral/ce.php?'.$query);
+        if($response->getStatusCode() != 200){
+            return ['msg' => 'Error on Server'];
+        }
+        $body = $response->getBody();
         $crawler = new Crawler((string) $body);
         $text = utf8_decode($crawler->text());
         $text = explode("\n", $text);
         $data =  [];
         foreach ($text as $line){
             $line = trim($line);
-            if($line != ""){
+            if($line != "" && $line != "Cerrar" && $line != "Imprimir"){
                 array_push($data,$line);
             }
         }
-        return $data;
 
+        if($data[1] == "ADVERTENCIA"){
+            $output = [
+                "result" => "Failed",
+                "msg" => $data[4]
+            ];
+
+        }else{
+            $output = $data;
+        }
+
+
+        return $output;
+
+    }
+
+    private function cleanDni($dni){
+        return preg_replace('/[^0-9]/', '', $dni);
+    }
+
+    private function completeDni($dni){
+        return str_pad($dni, 8, "0", STR_PAD_LEFT);
+    }
+
+    private function cleanCitizenship($citizenship){
+
+        return preg_replace('/[^VEJ]/', '', substr($citizenship, 0, 1));
     }
 
 }
