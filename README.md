38f9721e58314e2aee3476863541c9937c2cd927
Alexis Sanchez <aasanchez@gmail.com>
2016-07-19 11:37:12 +0200

    Big changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# aasanchez/cne
+
 
 [![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/aasanchez/cne/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/aasanchez/cne/?branch=master)
 [![Code Coverage](https://scrutinizer-ci.com/g/aasanchez/cne/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/aasanchez/cne/?branch=master)
@@ -8,3 +8,25 @@
 [![License](https://poser.pugx.org/aasanchez/cne/license)](https://packagist.org/packages/aasanchez/cne)
 [![SensioLabsInsight](https://insight.sensiolabs.com/projects/b090a912-009b-4b51-a9c0-0095dbdd38da/mini.png)](https://insight.sensiolabs.com/projects/b090a912-009b-4b51-a9c0-0095dbdd38da)
 [![Build Status](https://travis-ci.org/aasanchez/cne.svg?branch=master)](https://travis-ci.org/aasanchez/cne)
+
+NOT READY FOR PRODUCTION, THIS PACKAGE ITS UNDER DEVELOP
+
+# aasanchez/cne
+
+This Package that allows you to get CNE information in a json format.
+
+## Installation
+
+Add aasanchez/cne as a require dependency in your composer.json file:
+
+composer require aasanchez/cne
+
+## Usage
+
+Create a CNE Client instance:
+
+use Goutte\Client;
+
+$client = new Client();
+Make requests with the request() method:
+
--- src/cne.php
+++ src/cne.php
@@ -2,67 +2,56 @@
 
 namespace CNE;
 
-use GuzzleHttp\Client as Guzzle;
-use Symfony\Component\DomCrawler\Crawler as Crawler;
+use Goutte\Client;
 
 class Cne
 {
+    private $citizenship;
+
+    private $dniNumber;
 
     function __construct()
     {
+        $a = func_get_args();
+        $i = func_num_args();
+        if (method_exists($this, $f = '__construct'.$i)) {
+            call_user_func_array(array($this, $f), $a);
+        }
     }
 
-    public function search($citizenship,$dni)
+    function __construct1($a1)
     {
 
-        $dni = $this->cleanDni($dni);
-        $dni = $this->completeDni($dni);
-
-        $citizenship=$this->cleanCitizenship($citizenship);
-        $query = http_build_query(["nacionalidad" => $citizenship, "cedula" => $dni]);
-        $output = [];
-        $client = new Guzzle();
-        $response = $client->request('GET', 'http://www.cne.gov.ve/web/registro_electoral/ce.php?'.$query);
-        if($response->getStatusCode() != 200){
-            return ['msg' => 'Error on Server'];
-        }
-        $body = $response->getBody();
-        $crawler = new Crawler((string) $body);
-        $text = utf8_decode($crawler->text());
-        $text = explode("\n", $text);
-        $data =  [];
-        foreach ($text as $line){
-            $line = trim($line);
-            if($line != "" && $line != "Cerrar" && $line != "Imprimir"){
-                array_push($data,$line);
-            }
-        }
-
-        if($data[1] == "ADVERTENCIA"){
-            $output = [
-                "result" => "Failed",
-                "msg" => $data[4]
-            ];
-
-        }else{
-            $output = $data;
-        }
-
-
-        return $output;
-
     }
 
-    private function cleanDni($dni){
+    function __construct2($a1, $a2)
+    {
+        $this->citizenship = $a1;
+        $this->dniNumber = $a2;
+    }
+
+    public function search()
+    {
+        $client = new Client();
+
+        $crawler = $client->request('GET', 'http://www.cne.gob.ve/consultamovil?tipo=RE&nacionalidad=&cedula=15777118');
+
+        return $crawler;
+    }
+
+
+    private function cleanDni($dni)
+    {
         return preg_replace('/[^0-9]/', '', $dni);
     }
 
-    private function completeDni($dni){
+    private function completeDni($dni)
+    {
         return str_pad($dni, 8, "0", STR_PAD_LEFT);
     }
 
-    private function cleanCitizenship($citizenship){
-
+    private function cleanCitizenship($citizenship)
+    {
         return preg_replace('/[^VEJ]/', '', substr($citizenship, 0, 1));
     }
 
