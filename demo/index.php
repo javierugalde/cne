621bc9dcb6cae1c2a56e2c0a36789188b2a078d2
Alexis Sanchez <aasanchez@gmail.com>
2016-08-04 18:15:45 +0200

    Improves demo

--- demo/index.php
+++ demo/index.php
@@ -3,5 +3,33 @@
 require "../vendor/autoload.php";
 use aasanchez\Cne;
 
-$elector = new Cne('V', '15111444');
+$citizens = ["V", "E"];
+shuffle($citizens);
+$dni = rand ( 1, 100000000);
+$elector = new Cne($citizens[0], $dni);
 echo $elector->search()."\n";
+
+$citizens = ["V", "E"];
+shuffle($citizens);
+$dni = rand ( 1, 100000000);
+$elector = new Cne($citizens[0], $dni);
+echo $elector->search()."\n";
+
+$citizens = ["V", "E"];
+shuffle($citizens);
+$dni = rand ( 1, 100000000);
+$elector = new Cne($citizens[0], $dni);
+echo $elector->search()."\n";
+
+$citizens = ["V", "E"];
+shuffle($citizens);
+$dni = rand ( 1, 100000000);
+$elector = new Cne($citizens[0], $dni);
+echo $elector->search()."\n";
+
+$citizens = ["V", "E"];
+shuffle($citizens);
+$dni = rand ( 1, 100000000);
+$elector = new Cne($citizens[0], $dni);
+echo $elector->search()."\n";
+
