3e5713de7520500338ea313a654ddd57a2e19efc
Alexis Sanchez <aasanchez@gmail.com>
2016-08-07 14:56:18 +0200

    Update

--- src/Cne.php
+++ src/Cne.php
@@ -35,12 +35,18 @@ class Cne
     {
         $data = json_decode($data);
         var_dump($data);
+        $elector = [];
         $elector["datos-personales"]["cedula"] = $this->clear($data->ci);
         $elector["datos-personales"]["nombre(s)"] = $this->clear($data->nb1 . " " . $data->nb2);
         $elector["datos-personales"]["apellidos(s)"] = $this->clear($data->ap1 . " " . $data->ap2);
         $elector["datos-personales"]["nacimiento"] = $this->clear($data->fecha_nacimiento);
         $elector["informacion-electoral"]["centro-de-votacion"]["institucion"] = $this->clear($data->cv);
         $elector["informacion-electoral"]["centro-de-votacion"]["direccion"] = $this->clear($data->dir);
+        $elector["informacion-electoral"]["centro-de-votacion"]["parroquia"] = $this->parroquia($this->clear($data->par));
+        $elector["informacion-electoral"]["centro-de-votacion"]["municipio"] = $this->municipio($this->clear($data->mcp));
+        $elector["informacion-electoral"]["centro-de-votacion"]["estado"] = $this->estado($this->clear($data->stdo));
+        $elector["informacion-electoral"]["centro-de-votacion"]["st"] = $this->clear($data->st);
+        $elector["ultima-actualizacion"]= $this->clear($data->fecha);
         return json_encode($elector);
     }
     private function clear($string){
@@ -50,4 +56,15 @@ class Cne
         return $newString;
     }
 
+    private function parroquia($parroquia){
+        return str_replace("Pq. ", "", $parroquia);
+    }
+
+    private function municipio($parroquia){
+        return str_replace("Ce. ", "", $parroquia);
+    }
+
+    private function estado($parroquia){
+        return str_replace("Edo. ", "", $parroquia);
+    }
 }
