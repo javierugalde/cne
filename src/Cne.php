e14af2c6f27ea3cac2a68bd3f11e72b0f9635af5
Alexis Sanchez <aasanchez@gmail.com>
2016-08-07 13:50:14 +0200

    Add

--- src/Cne.php
+++ src/Cne.php
@@ -28,6 +28,26 @@ class Cne
                 'cedula' => $this->dniNumber
             ]
         ]);
-        return utf8_encode($this->info->getBody());
+        return $this->formatter($this->info->getBody());
     }
+
+    private function formatter($data)
+    {
+        $data = json_decode($data);
+        var_dump($data);
+        $elector["datos-personales"]["cedula"] = $this->clear($data->ci);
+        $elector["datos-personales"]["nombre(s)"] = $this->clear($data->nb1 . " " . $data->nb2);
+        $elector["datos-personales"]["apellidos(s)"] = $this->clear($data->ap1 . " " . $data->ap2);
+        $elector["datos-personales"]["nacimiento"] = $this->clear($data->fecha_nacimiento);
+        $elector["informacion-electoral"]["centro-de-votacion"]["institucion"] = $this->clear($data->cv);
+        $elector["informacion-electoral"]["centro-de-votacion"]["direccion"] = $this->clear($data->dir);
+        return json_encode($elector);
+    }
+    private function clear($string){
+        $newString = filter_var($string, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+        $newString = strtolower($newString);
+        $newString = ucfirst($newString);
+        return $newString;
+    }
+
 }
