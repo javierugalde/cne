3c5aba2bbb6878c408138f6b7af8cb0eac3e36d0
Alexis Sánchez <aasanchez@gmail.com>
2017-09-20 11:22:07 +0200

    Multi search

--- demo/index.php
+++ demo/index.php
@@ -1,23 +1,13 @@
 <?php
 
 require '../vendor/autoload.php';
+
 use aasanchez\Cne;
 
-$elector = new Cne('V', '7217609');
-header('Content-Type: application/json');
-echo $elector->searchPretty();
-echo PHP_EOL;
-$elector = new Cne('V', '24234355');
-echo $elector->searchPretty();
-echo PHP_EOL;
-$elector = new Cne('V', '4308005');
-echo $elector->searchPretty();
-echo PHP_EOL;
-$elector = new Cne('V', '16666860');
-echo $elector->searchPretty();
-echo PHP_EOL;
-$elector = new Cne('V', '5892464');
-echo $elector->searchPretty();
-echo PHP_EOL;
-$elector = new Cne('V', '12160718');
-echo $elector->searchPretty();
+$cedulas = ['7217609', '24234355', '4308005', '16666860', '5892464', '12160718'];
+foreach ($cedulas as $cedula){
+
+    $elector = new Cne('V', $cedula);
+    echo  PHP_EOL.$elector->searchPretty();
+
+}
